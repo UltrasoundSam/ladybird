ec6cbe18ed3489e383d14d8f65b12a127f6e006c
Sam Hill <s.hill@yorksj.ac.uk>
2026-01-19 13:45:31 +0000

    Adds in count of number of steps per simulation before stopping

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,31 @@ use rayon::prelude::*;
 
 const N: i32 = 12;
 
-fn run_iteration<R: Rng + ?Sized>(rng: &mut R) -> i32 {
+fn run_iteration<R: Rng + ?Sized>(rng: &mut R) -> (i32, usize) {
     let mut current_location: i32 = 0;
     let mut visited: HashSet<i32> = HashSet::new();
     visited.insert(current_location);
-
+    let mut steps = 0;
     let directions = [-1, 1];
 
     while visited.len() < (N as usize - 1) {
 
         let step = directions[rng.random_range(0..2)];
         current_location = (current_location + step).rem_euclid(N);
-	visited.insert(current_location);
+	    visited.insert(current_location);
+        steps += 1;
     }
 
-    for i in 0..N {
-        if !visited.contains(&i) {
-            return i;
-        }
-    }
-    unreachable!("There must be exactly one unvisited element");
+    let last_item = (0..N).find(|i| !visited.contains(i)).unwrap();
+    (last_item, steps)
 }
-
 
 
 fn main() {
     let iterations = 1_000_000;
 
     // Create a iterator over the iterations
-    let results: Vec<i32> = (0..iterations)
+    let results: Vec<(i32, usize)> = (0..iterations)
         .into_par_iter()
         .map(|_| {
             let mut rng = rand::rng();
@@ -43,8 +39,11 @@ fn main() {
 
     // Count occurrences
     let mut counts = [0usize; 12];
-    for v in results {
-        counts[v as usize] += 1;
+    let mut steps_total: usize = 0;
+
+    for (last, steps) in results {
+        counts[last as usize] += 1;
+        steps_total += steps;
     }
 
     // Percentages (1–11, renormalized)
@@ -54,4 +53,10 @@ fn main() {
         let pct = (*c as f64) * 100.0 / (total_nonzero as f64);
         println!("{:2}: {:6.3}%", i, pct);
     }
+
+    let average_steps = steps_total as f64 / iterations as f64;
+    println!(
+        "\nAverage steps before only one number remains unvisited: {:.2}",
+        average_steps
+    );
 }
