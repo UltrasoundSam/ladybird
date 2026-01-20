254e13e87d6647ecefdfcc34542a8fcc995263e6
Sam Hill <s.hill@yorksj.ac.uk>
2026-01-20 10:57:27 +0000

    Adds in some comments to main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,31 @@ use std::collections::HashSet;
 use rayon::prelude::*;
 
 
-const N: i32 = 12;
+const NUM_HOURS: i32 = 12;
 
 fn run_iteration<R: Rng + ?Sized>(rng: &mut R) -> (i32, usize) {
     let mut current_location: i32 = 0;
+
+    // Keep track of visited numbers with hashset
     let mut visited: HashSet<i32> = HashSet::new();
     visited.insert(current_location);
+
+    // Also want to know how many steps we have taken before we stop
     let mut steps = 0;
+
     let directions = [-1, 1];
 
-    while visited.len() < (N as usize - 1) {
-
+    // Keep random walking (50 % chance moving + or -) until we have visited 11 separate
+    // locations
+    while visited.len() < (NUM_HOURS as usize - 1) {
         let step = directions[rng.random_range(0..2)];
-        current_location = (current_location + step).rem_euclid(N);
+        current_location = (current_location + step).rem_euclid(NUM_HOURS);
 	    visited.insert(current_location);
         steps += 1;
     }
 
-    let last_item = (0..N).find(|i| !visited.contains(i)).unwrap();
+    // Now we have stopped, let's find which value is i is NOT in hashset
+    let last_item = (0..NUM_HOURS).find(|i| !visited.contains(i)).unwrap();
     (last_item, steps)
 }
 
@@ -28,7 +35,7 @@ fn run_iteration<R: Rng + ?Sized>(rng: &mut R) -> (i32, usize) {
 fn main() {
     let iterations = 1_000_000;
 
-    // Create a iterator over the iterations
+    // Use parallel looping since everything is independent
     let results: Vec<(i32, usize)> = (0..iterations)
         .into_par_iter()
         .map(|_| {
@@ -37,7 +44,7 @@ fn main() {
         })
         .collect();
 
-    // Count occurrences
+    // Count occurrences of each stopping number and length of each simulation
     let mut counts = [0usize; 12];
     let mut steps_total: usize = 0;
 
@@ -54,7 +61,8 @@ fn main() {
         println!("{:2}: {:6.3}%", i, pct);
     }
 
-    let average_steps = steps_total as f64 / iterations as f64;
+    // Calculate average steps
+    let average_steps: f64 = steps_total as f64 / iterations as f64;
     println!(
         "\nAverage steps before only one number remains unvisited: {:.2}",
         average_steps
